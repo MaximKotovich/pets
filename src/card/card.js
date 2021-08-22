35630d97692bca57a5d9f99863799bf97ae3fa6f
MaximKotovich <makckotok2020@gmail.com>
2021-08-22 15:57:20 +0300

    update

--- src/card/card.js
+++ src/card/card.js
@@ -3,20 +3,25 @@ import ReactDOM from "react-dom";
 import "./card.css";
 
 export default function Card({ catsArr }) {
+  const [cat, setCat] = useState(true);
+  const [dog, setDog] = useState(false);
 
-
-  const catsCard = catsArr.map((item) => {     
-
-
+  const catsCard = catsArr.map((item) => {
+    console.log(cat);
     return (
-      <div className={item.id % 2 == 0 ? "firstContent reverse" : "firstContent"} key = {item.id}>
-        <div className="infoFor">
-          <p>{item.title}</p>
-          <p>{item.description}</p>
-          <button>Узнать подробнее</button>
+     
+      <div className={cat ? "preFirst activeContent" : "preFirst"}>
+        <div
+          className={item.id % 2 == 0 ? "firstContent reverse" : "firstContent"}
+          key={item.id}
+        >
+          <div className="infoFor">
+            <p>{item.title}</p>
+            <p>{item.description}</p>
+            <button>Узнать подробнее</button>
+          </div>
+          <div className="imgCard">{item.imgUrl}</div>
         </div>
-        <div className="imgCard">{item.imgUrl}</div>
-      
       </div>
     );
   });
@@ -24,10 +29,26 @@ export default function Card({ catsArr }) {
   return (
     <div className="containerCard">
       <div className="titleCard">
-        <div className="firstTitle activeTitleCard">Котики</div>
-        <div className="secTitle">Собачки</div>
+        <div
+          className={cat ? "firstTitle activeTitleCard" : "firstTitle"}
+          onClick={() => {
+            setCat(true);
+            setDog(false);
+          }}
+        >
+          Котики
+        </div>
+        <div
+          className={dog ? "secTitle activeTitleCard" : "secTitle"}
+          onClick={() => {
+            setCat(false);
+            setDog(true);
+          }}
+        >
+          Собачки
+        </div>
       </div>
-      <div className="bodyContent">
+      <div className="bodyContentCard">
         {catsCard}
         <div className="secContent"></div>
       </div>
