a4381f7f9669553a3dd6589d8467c6e0896ae82c
MaximKotovich <makckotok2020@gmail.com>
2021-09-06 11:29:41 +0300

    update picture

--- src/content/content.js
+++ src/content/content.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./content.css";
-import pet from "./pets.png";
+import pet from "./Kot-sobaka.png";
 
 export default function Content() {
   return (
@@ -10,7 +10,7 @@ export default function Content() {
         <div className="contentBlock">
           <p className="title">Твой новый друг</p>
           <p className="bodyContent">
-            Онлайн-магазин домашних животных подарит тебе нового друга в
+            Онлайн-приют домашних животных подарит тебе нового друга в
             несколько кликов. Действуй!!!
           </p>
           <button>Посмотреть друзей</button>
